c6d0740bc06135494d9cdd6f3e2c45cfbb56f7ed
Frederik Reiter <frederik.reiter@hotmail.de>
2017-11-04 16:16:02 +0100

    Revert "Revert "Removed reference to self in commands __init""
    This reverts commit 344f5cb29dd411c8c8e1b83a5bd004828faee0f0.

--- commands/translate.py
+++ commands/translate.py
@@ -10,12 +10,12 @@ class Translate(Command):
     def __init__(self):
 
         # Information about the command
-        self.aliases = ["translate"]
-        self.shortdescription = "Quickly translate stuff"
-        self.longdescription = "Translate a string into another language. The command uses the free Yandex Translate " \
+        aliases = ["translate"]
+        shortdescription = "Quickly translate stuff"
+        longdescription = "Translate a string into another language. The command uses the free Yandex Translate " \
                                "API: https://translate.yandex.com/. Works best with single words. "
-        self.usage = "/".join(self.aliases) + " <detect|<lang (e.g. de-en)>|available> [text]"
-        self.category = Category.UTIL
+        usage = "/".join(self.aliases) + " <detect|<lang (e.g. de-en)>|available> [text]"
+        category = Category.UTIL
 
         # Get the key from config.json
         with open('config.json') as data_file:
@@ -24,7 +24,7 @@ class Translate(Command):
 
         # Create new Translate instance
         self.translate = YandexTranslate(self.yandexkey)
-        super().__init__(self.aliases , self.shortdescription , self.longdescription , self.usage , self.category)
+        super().__init__(aliases , shortdescription , longdescription , usage , category)
 
     # Called by CommandHandler when the command is executed
     async def execute(self , client , message , args , **kwargs):
